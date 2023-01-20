b25cca6bf163a3801afa90f7ce6a72632a5e03e0
Mukul Dhir <dhirmukul3@gmail.com>
2023-01-20 22:22:14 +0000

    sm7235-common: Drop LiveDisplay

--- sm7325.mk
+++ sm7325.mk
@@ -251,10 +251,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.1.vendor
 
-# LiveDisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.1-service.motorola_lahaina
-
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/media/media_codecs_yupik_v0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_yupik_v0.xml \
